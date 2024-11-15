aaf6ae40906bb5923996dc607bafab82475236bf
zaheer-bashir <zaheerkpr3@gmail.com>
2024-11-15 23:48:39 +0500

    Font Optmization - 2

--- src/ui/Home/EventShowcase.jsx
+++ src/ui/Home/EventShowcase.jsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 const EventShowcase = () => {
   return (
     <div className="flex flex-col items-center text-center gap-16 pt-12 lg:pt-8 pb-14 px-6">
-      <h2 className="uppercase text-[32px] sm:text-[36px] lg:text-[44px] max-w-[1100px] !leading-[1.1em] font-italiana font-semibold">
+      <h2 className="uppercase text-[22px] sm:text-[36px] lg:text-[44px] max-w-[1100px] !leading-[1.1em] font-semibold">
         extraordinary experiences, event design, and production in the heart of{" "}
-        <span
-          className="font-qwitcher text-[2.5em] leading-[0.4em] translate-y-[0.05em] inline-block"
+        <p
+          className="text-[1em] leading-[0.4em] translate-y-[0.05em] mt-10"
           style={{ textTransform: "none" }}
         >
           San Diego and LA
-        </span>
+        </p>
       </h2>
 
       <div className="flex flex-col w-full gap-10">
