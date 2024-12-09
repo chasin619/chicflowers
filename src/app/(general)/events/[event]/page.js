4946a64023bc5de71fa32dca11b5a3bbc6e0ead9
zaheer-bashir <zaheerkpr3@gmail.com>
2024-12-09 23:45:32 +0500

    Fix: Fix Portfolio Crash

--- src/app/(general)/events/[event]/page.js
+++ src/app/(general)/events/[event]/page.js
@@ -2,24 +2,25 @@
 
 import { useParams } from "next/navigation";
 import React from "react";
-import { portfolioData } from "@/data/home";
+import useHomeStore from "@/store/home";
 
 const Event = () => {
   const params = useParams();
+  const { portfolios } = useHomeStore();
 
-  const eventData = portfolioData[params.event - 1];
+  const eventData = portfolios[params.event - 1];
 
   return (
     <div className="flex flex-col px-[5vw] pt-[72px] items-center">
       <h1 className="font-italiana text-[40px] xs:text-5xl lg:text-6xl font-bold my-[1.066em] text-center !leading-none">
-        {eventData.venue}
+        {eventData.title}
       </h1>
 
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-[3.333vw] [grid-auto-rows:500px]">
         {eventData.images.map((image, i) => (
           <div key={i}>
             <img
-              src={image.src}
+              src={image}
               className="w-full h-full object-cover rounded-md"
             />
           </div>
