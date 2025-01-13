8f16ab62e447cade34d94718ab05723cf10f82d6
zaheer-bashir <zaheerkpr3@gmail.com>
2025-01-14 01:00:13 +0500

    Feat: improve SEO metadata

--- src/app/(general)/blog/[slug]/page.tsx
+++ src/app/(general)/blog/[slug]/page.tsx
@@ -1,31 +1,87 @@
-"use client";
-import { useEffect } from "react";
-
+import axios from "axios";
 import HeroSection from "@/ui/BlogDetails/HeroSecion";
 import ContentSection from "@/ui/BlogDetails/ContentSection";
-import useHomeStore from "@/store/home";
+import { Metadata } from "next";
+import { BASE_URL } from "@/utils/constants";
+import Head from "next/head";
 
-const BlogDetails = ({ params }: { params: { slug: string } }) => {
-  const { blogs, getAllBlogs } = useHomeStore();
-  const slug = params.slug;
-  const post = blogs.find((post) => post.slug === slug);
+async function getBlogBySlug(slug: string) {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/blog/slug/?slug=${slug}`);
+    return response.data.blog;
+  } catch (error) {
+    console.error("Error fetching blog data:", error.response.data);
+    return null;
+  }
+}
 
-  useEffect(() => {
-    if (!post) {
-      getAllBlogs();
-    }
-  }, []);
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const post = await getBlogBySlug(params.slug);
+  return {
+    title: post?.title,
+    description: post?.meta_description,
+    openGraph: {
+      title: post?.title,
+      description: post?.meta_description,
+      type: "article",
+      url: `https://chicflowers.com/blog/${params.slug}`,
+      images: [
+        {
+          url: post?.image,
+          width: 1200,
+          height: 630,
+          alt: post?.title,
+        },
+      ],
+      siteName: "CHIC Flowers",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post?.title,
+      description: post?.meta_description,
+      images: [post?.image || "https://chicflowers.com/twitter.png"],
+      creator: "@alona_chasin",
+    },
+    metadataBase: new URL("https://chicflowers.com/"),
+    alternates: {
+      canonical: `https://chicflowers.com/blog/${params.slug}`,
+    },
+  };
+}
+
+const BlogDetailsPage = async ({ params }: { params: { slug: string } }) => {
+  const post = await getBlogBySlug(params.slug);
 
   if (!post) {
-    return <></>;
+    return <div>Blog post not found.</div>;
   }
 
   return (
     <div>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.meta_description} />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.meta_description} />
+        <meta property="og:image" content={post.image} />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:url"
+          content={`https://chicflowers.com/blog/${post.slug}`}
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={post.title} />
+        <meta name="twitter:description" content={post.meta_description} />
+        <meta name="twitter:image" content={post.image} />
+      </Head>
       <HeroSection post={post} />
       <ContentSection htmlContent={post.content} />
     </div>
   );
 };
 
-export default BlogDetails;
+export default BlogDetailsPage;
